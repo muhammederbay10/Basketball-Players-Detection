81e040973727ec4fc331cb54e9ae13f0dc2acee3
Muhammed Erbay <muhammederbay2003@gmail.com>
2025-07-06 07:48:26 +0300

    Update predict.py to run inference on a test video and adjust confidence and IoU parameters

--- scripts/predict.py
+++ scripts/predict.py
@@ -8,9 +8,15 @@ model = YOLO("runs/detect/train12/weights/best.pt")
 image_path = "data/basketball-player-detection.v4i.yolov8/test/images/suggested-OkHELcZMcDjRYc8Cx3U9_jpg.rf.4c6600d5d7ef15b4b4a13a2917f3dda2.jpg"
 image = cv2.imread(image_path)
 
+# Inference o test video
+video_path = "data/basketball-player-detection.v4i.yolov8/test/Videos/training.mp4"
+
 # Run inference
-results = model.predict(source=image, save=True, conf=0.25)
+results = model.predict(source=video_path, conf=0.6, iou=0.5, save=True)
 
 # Plot Predcitions on the image
 annotated_img = results[0].plot()
 print("Prediction saved in: ", results[0].save_dir)
+results[0].save_crop("output/")
+results[0].show
+results[0].save("output/image.jpg")
